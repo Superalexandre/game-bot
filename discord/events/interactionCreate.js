d8d13d1ce9c74808c05196f2e4b8e137e4b4454d
Superalexandre <superalexandre0@gmail.com>
2021-08-08 13:03:32 +0200

    [*] v13 and slashcommand

--- discord/events/interactionCreate.js
+++ discord/events/interactionCreate.js
@@ -1,3 +1,5 @@
+const i18n = require("i18n")
+
 module.exports = class interactionCreate {
     constructor(client) {
         this.client = client
@@ -5,9 +7,57 @@ module.exports = class interactionCreate {
 
     async run(interaction) {
         const client = this.client
+        const data = client.data
+        const start = Date.now()
 
         if (!interaction.isCommand()) return
 
-        console.log(interaction)
+        const userData = {} //await client.data.users.get(message.author.accountID)
+
+        i18n.setLocale("fr_FR")
+
+        const cmd = client.commands.get(interaction.commandName) || client.commands.get(client.aliases.get(interaction.commandName))
+
+        if (!cmd) return interaction.reply({
+            content: "Commande introuvable",
+            ephemeral: true, 
+            allowedMentions: { repliedUser: false }
+        })
+
+        try {
+            const messageTime = (Date.now() - start) / 1000
+        
+            cmd.run({
+                client: client,
+                interaction: interaction,
+                options: interaction.options,
+                i18n: i18n,
+                data: data,
+                userData: userData,
+        
+                util: {
+                    messageTimeProcessing: messageTime,
+                    startMessageProcessing: start
+                }
+            }).then(() => {
+                client.logger.commandLog({ message: cmd.help.name } /*message, prefix, cmd.help.name, messageTime, ((Date.now() - start) / 1000) - messageTime*/)
+            }).catch(error => {
+                interaction.reply({ 
+                    content: i18n.__("error.errorOccured", { error: error.toString() }), 
+                    ephemeral: true,
+                    allowedMentions: { repliedUser: false } 
+                })
+        
+                client.logger.error({ message: error })
+            })
+        } catch(error) {
+            interaction.reply({ 
+                content: i18n.__("error.errorOccured", { error: error.toString() }), 
+                ephemeral: true,
+                allowedMentions: { repliedUser: false } 
+            })
+        
+            client.logger.error({ message: error })
+        }
     }
 }
