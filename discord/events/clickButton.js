605676b755bb455c25cc18a41822f75801de8f17
Superalexandre <superalexandre0@gmail.com>
2021-08-27 19:39:57 +0200

    [*] Bug fix

--- discord/events/clickButton.js
+++ discord/events/clickButton.js
@@ -4,21 +4,14 @@ module.exports = class clickButton {
     }
 
     async run(button) {
-        console.log(2)
-
         const client = this.client
         const id = button.customId.split("_")
-
-        return true
-        //Error
 
         if (id[1] === "uno" && id.includes("ephemeral")) {
             const gameID = id[3]
             const command = client.commands.get(id[1])
 
             command.playCard({ client, gameID, button })
-
-            //client.games.uno.get("1")
         }
     }
 }
