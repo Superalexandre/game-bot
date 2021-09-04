d5d8f5cbd06b2f01e95566fc400b23a243f89fcf
Superalexandre <superalexandre0@gmail.com>
2021-09-04 11:51:50 +0200

    [*] Bug fix and improve sudoku

--- discord/commands/sudoku.js
+++ discord/commands/sudoku.js
@@ -14,6 +14,10 @@ module.exports = class Sudoku extends Command {
         const difficulty = options.getString("difficulte")
         const board = await sudoku.generate(difficulty)
         const grid = sudoku.board_string_to_grid(board)
+
+        const resultString = sudoku.solve(board)
+        const resultGrid = sudoku.board_string_to_grid(resultString)
+
         const emotes = {
             "blue": {
                 ".": "<:B0:883431754231345193>",
@@ -40,10 +44,37 @@ module.exports = class Sudoku extends Command {
                 "8": "<:V8:883457698316824576>",
                 "9": "<:V9:883457698245533770>",
                 "space": "⬜"
+            },
+            "yellow": {
+                ".": "<:J0:883467881415860254>",
+                "1": "<:J1:883467881369718864>",
+                "2": "<:J2:883467880920911945>",
+                "3": "<:J3:883467881319383080>",
+                "4": "<:J4:883467881336164402>",
+                "5": "<:J5:883467881386496050>",
+                "6": "<:J6:883467881327771648>",
+                "7": "<:J7:883467881235484763>",
+                "8": "<:J8:883467881357123604>",
+                "9": "<:J9:883467881378091038>",
+                "space": "⬜"
+            },
+            "red": {
+                ".": "<:R0:883634258957197352>",
+                "1": "<:R1:883634090140647434>",
+                "2": "<:R2:883634089889001532>",
+                "3": "<:R3:883634089868030002>",
+                "4": "<:R4:883634089863819274>",
+                "5": "<:R5:883634089943519262>",
+                "6": "<:R6:883634089595396108>",
+                "7": "<:R7:883634090312597595>",
+                "8": "<:R8:883634089830273055>",
+                "9": "<:R9:883634089905778728>",
+                "space": "⬜"
             }
         }
 
         let playerInfo = {
+            difficulty: difficulty,
             line: undefined,
             column: undefined,
             number: undefined,
@@ -54,7 +85,7 @@ module.exports = class Sudoku extends Command {
 
         const embed = new MessageEmbed()
             .setTitle("Sudoku de " + interaction.user.username)
-            .setDescription(`Ligne : ${playerInfo.line ?? "Pas séléctionner"}\nColonne : ${playerInfo.column ?? "Pas séléctionner"}\n\n` + gameInfo.string)
+            .setDescription(`Difficulté : ${playerInfo.difficulty}\nLigne : ${playerInfo.line ?? "Pas sélectionnée"}\nColonne : ${playerInfo.column ?? "Pas sélectionnée"}\nChiffre : ${playerInfo.column && playerInfo.line ? "séleectionnée le" : "A choisir après"}\n\n` + gameInfo.string + `${playerInfo.error ? `\n${playerInfo.error}` : ""}`)
 
         const msg = await interaction.channel.send({
             embeds: [embed],
@@ -72,6 +103,8 @@ module.exports = class Sudoku extends Command {
                 ephemeral: true
             })
 
+            const mainText = (playerInfo, gameInfo) => `Difficulté : ${playerInfo.difficulty}\nLigne : ${playerInfo.line ?? "Pas sélectionnée"}\nColonne : ${playerInfo.column ?? "Pas sélectionnée"}\nChiffre : ${playerInfo.column && playerInfo.line ? "séleectionnée le" : "A choisir après"}\n\n` + gameInfo.string + `${playerInfo.error ? `\n${playerInfo.error}` : ""}`
+
             const [,,, idNumber] = button.customId.split("_")
             const number = parseInt(idNumber)
     
@@ -87,7 +120,52 @@ module.exports = class Sudoku extends Command {
 
                 const embed = new MessageEmbed()
                     .setTitle("Sudoku de " + interaction.user.username)
-                    .setDescription(`Ligne : ${playerInfo.line ?? "Pas sélectionnée"}\nColonne : ${playerInfo.column ?? "Pas sélectionnée"}\nChiffre : ${playerInfo.column && playerInfo.line ? "séleectionnée le" : "A choisir après"}\n\n` + gameInfo.string + `${playerInfo.error ? `\n${playerInfo.error}` : ""}`)
+                    .setDescription(mainText(playerInfo, gameInfo))
+
+                return await msg.edit({
+                    embeds: [embed],
+                    components: gameInfo.components
+                })
+            }
+
+            //* Check
+            if (idNumber === "check") {
+                let win = true
+
+                for (let i = 0; i < resultGrid.length; i++) {
+                    for (let j = 0; j < resultGrid[i].length; j++) {
+                        const value = grid[i][j].split("-")[0]
+
+                        if (value !== resultGrid[i][j]) {
+                            win = false
+                            grid[i][j] = `${value}-false`
+                        } else grid[i][j] = resultGrid[i][j]
+                    }
+                }
+
+                if (win) {
+                    const gameInfo = await genInfo({ grid, emotes, interaction, highlightedLine: playerInfo.line, highlightedColumn: playerInfo.column })
+
+                    await button.deferUpdate()
+
+                    const embed = new MessageEmbed()
+                        .setTitle("Sudoku de " + interaction.user.username)
+                        .setDescription(`Bravo ${interaction.user.username} vous avez gagner en difficulté ${playerInfo.difficulty} !\n\n` + gameInfo.string)
+
+                    
+                    return await msg.edit({
+                        embeds: [embed],
+                        components: []
+                    })
+                }
+
+                const gameInfo = await genInfo({ grid, emotes, interaction, highlightedLine: playerInfo.line, highlightedColumn: playerInfo.column })
+
+                await button.deferUpdate()
+
+                const embed = new MessageEmbed()
+                    .setTitle("Sudoku de " + interaction.user.username)
+                    .setDescription(mainText(playerInfo, gameInfo))
 
                 return await msg.edit({
                     embeds: [embed],
@@ -97,24 +175,43 @@ module.exports = class Sudoku extends Command {
 
             playerInfo.error = ""
 
+            if ((!playerInfo.line || !playerInfo.column) && idNumber === "void") {
+                playerInfo.error = "Ce n'est pas le moment de vider une case"
+
+                const gameInfo = await genInfo({ grid, emotes, interaction, highlightedLine: playerInfo.line, highlightedColumn: playerInfo.column })
+
+                await button.deferUpdate()
+
+                const embed = new MessageEmbed()
+                    .setTitle("Sudoku de " + interaction.user.username)
+                    .setDescription(mainText(playerInfo, gameInfo))
+
+                return await msg.edit({
+                    embeds: [embed],
+                    components: gameInfo.components
+                })
+            }
+
             //* Check where add number
             if (!playerInfo.line) {
                 playerInfo.line = number
             } else if (!playerInfo.column) {
                 playerInfo.column = number
-            } else playerInfo.number = number
+            } else playerInfo.number = idNumber === "void" ? "void" : number
 
             //* If all complete
             if (playerInfo.line && playerInfo.column) {
                 //* If already occuped but base
-                if (grid[playerInfo.line - 1][playerInfo.column - 1] !== "." && !grid[playerInfo.line - 1][playerInfo.column - 1].endsWith("-placed")) {              
+                const type = grid[playerInfo.line - 1][playerInfo.column - 1].split("-")
+
+                if (grid[playerInfo.line - 1][playerInfo.column - 1] !== "." && (!type[1] && !["false", "placed"].includes(type[1]))) {              
                     playerInfo.column = undefined
                     playerInfo.error = "Désolé mais vous ne pouvez pas choisir cette case"
                 }
 
                 //* If all good
                 if (playerInfo.number) {
-                    grid[playerInfo.line - 1][playerInfo.column - 1] = `${number}-placed`
+                    grid[playerInfo.line - 1][playerInfo.column - 1] = idNumber === "void" ? "." : `${number}-placed`
                     playerInfo.line = undefined
                     playerInfo.column = undefined
                     playerInfo.number = undefined
@@ -127,11 +224,21 @@ module.exports = class Sudoku extends Command {
 
             const embed = new MessageEmbed()
                 .setTitle("Sudoku de " + interaction.user.username)
-                .setDescription(`Ligne : ${playerInfo.line ?? "Pas sélectionnée"}\nColonne : ${playerInfo.column ?? "Pas sélectionnée"}\nChiffre : ${playerInfo.column && playerInfo.line ? "séleectionnée le" : "A choisir après"}\n\n` + gameInfo.string + `${playerInfo.error ? `\n${playerInfo.error}` : ""}`)
+                .setDescription(mainText(playerInfo, gameInfo))
+
+            const components = gameInfo.components
+            if (gameInfo.allFilled) {
+                const buttons = new MessageButton()
+                    .setCustomId(`game_sudoku_${interaction.user.id}_check`)
+                    .setLabel("Verifier")
+                    .setStyle("SUCCESS")
+
+                gameInfo.components[2].addComponents(buttons)
+            }
 
             await msg.edit({
                 embeds: [embed],
-                components: gameInfo.components
+                components: components
             })
         })
     }
@@ -157,10 +264,13 @@ async function genInfo({ grid, emotes, interaction, highlightedLine, highlighted
                 color = "green"
             }
 
-            // TODO : If placed => Yellow
-            const [value, placed] = grid[i][j].split("-")
+            let [value, type] = grid[i][j].split("-")
 
-            allFilled = value !== "."
+            color = type === "placed" ? "yellow" : type === "false" ? "red" : color
+
+            if (!isNaN(value)) value = parseInt(value)
+
+            if (allFilled) allFilled = value !== "."
 
             string += emotes[color][value]
         }
@@ -169,7 +279,7 @@ async function genInfo({ grid, emotes, interaction, highlightedLine, highlighted
     }
 
     //* Create all buttons
-    const [numberComponents1, numberComponents2] = [new MessageActionRow(), new MessageActionRow()]
+    const [numberComponents1, numberComponents2, numberComponents3] = [new MessageActionRow(), new MessageActionRow(), new MessageActionRow()]
 
     for (let i = 1; i < 11; i++) {
         const button = new MessageButton()
@@ -181,5 +291,13 @@ async function genInfo({ grid, emotes, interaction, highlightedLine, highlighted
         i - 1 >= 5 ? numberComponents2.addComponents(button) : numberComponents1.addComponents(button)
     }
 
-    return { string, components: [numberComponents1, numberComponents2] }
+    const nothing = new MessageButton()
+        .setCustomId(`game_sudoku_${interaction.user.id}_void`)
+        .setStyle("SECONDARY")
+        .setLabel("Vider la case")
+        .setDisabled(!highlightedLine || !highlightedColumn)
+
+    numberComponents3.addComponents(nothing)
+
+    return { string, components: [numberComponents1, numberComponents2, numberComponents3], allFilled }
 }
