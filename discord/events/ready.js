a72168ebee11d9d64ea37b4d67dc2c3fdea7ad0a
Superalexandre <superalexandre0@gmail.com>
2021-09-26 01:18:41 +0200

    [/] Fix critical problem

--- discord/events/ready.js
+++ discord/events/ready.js
@@ -7,18 +7,5 @@ export default class Ready {
         const client = this.client
 
         client.logger.log({ message: "Client connecté" })
-
-        //const headers = {
-        //    "Content-Type": "application/json",
-        //    "Authorization": "mfa.u_bJqaWbob31U30z1mj_lYQZFpH91UYD-2tk_7EBaMQN5TZcBkp3AJ4J8M57i5XKMLzTteKRj7yKHYrCe875"
-        //}
-        //
-        //const req = await fetch("https://discord.com/api/v9/users/@me", {
-        //    method: "PATCH",
-        //    headers: headers,
-        //    body: JSON.stringify({ bio: "TEST" })
-        //})
-        //
-        //console.log(await req.json())
     }
 }
