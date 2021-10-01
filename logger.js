22ede7fa0d80b1467bfd7458c5d92973741dd923
Alexandre <superalexandre0@gmail.com>
2021-10-01 22:30:46 +0200

    [+] Add new functions

--- logger.js
+++ logger.js
@@ -2,6 +2,10 @@
 //    { formateDate } = require("./discord/utils/functions"),
 //    logUpdate = require("log-update")
 
+import colors from "colors"
+import DateFns from "date-fns-tz"
+import { fr } from "date-fns/locale/index.js"
+
 import * as Sentry from "@sentry/node"
 export default class Logger {
     constructor(mode) {
@@ -9,20 +13,41 @@ export default class Logger {
     }
 
     log({ message }) {
-        return console.log(message)
+        if (this.mode === "compact") {
+            console.log(`[${formateDate(Date.now(), this.mode)}] ${green("[LOG]")} ${message}`)
+        }
     }
 
     error({ message }) {
-        Sentry.captureException(log)
+        Sentry.captureException(message)
 
-        return console.error(message)
+        if (this.mode === "compact") {
+            return console.log(`[${formateDate(Date.now(), this.mode)}] [ERROR] ${message}`)
+        }
     }
 
     warn({ message }) {
-        return console.warn(message)
+        Sentry.captureMessage(message)
+
+        if (this.mode === "compact") {
+            return console.log(`[${formateDate(Date.now(), this.mode)}] [WARN] ${message}`)
+        }
     }
 
     commandLog({ message }) {
-        return console.log(message)
+        if (this.mode === "compact") {
+            return console.log(`[${formateDate(Date.now(), this.mode)}] [LOG] ${message}`)
+        }
     }
+}
+
+function formateDate(date = Date.now(), type) {
+    const dateZonedTime = DateFns.utcToZonedTime(date, "Europe/Paris")
+
+    const formatDate = type === "compact" ? "dd'/'MM'/'yy pp" : "EEEE dd LLLL yyyy 'à' pp"
+
+    return DateFns.format(dateZonedTime, formatDate, {
+        timeZone: "Europe/Paris",
+        locale: fr
+    })
 }
