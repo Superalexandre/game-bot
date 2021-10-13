664e4d67ddc7c505987f1877676d50e61b39e333
Superalexandre <superalexandre0@gmail.com>
2021-10-13 19:51:11 +0200

    [+] Execute command

--- instagram/events/messageCreate.js
+++ instagram/events/messageCreate.js
@@ -10,68 +10,41 @@ export default class messageCreate {
         if (message.author.id === client.user.id) return
 
         const prefix = client.config.instagram.prefix
-        const command = message.content.split(" ")[0].slice(prefix).toLowerCase()
+        const command = message.content.split(" ")[0].slice(prefix.length).toLowerCase()
         const args = message.content.split(/ +/g).slice(1)
         const argsOptions = message.content.split(/--([a-z]+) ([a-z]+)/gm).slice(1)
+
+        const data = {}
+
+        i18n.setLocale("fr_FR")
 
         await message.markSeen()
 
         const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
 
-        console.log(cmd)
+        if (!cmd) return
+
+        if (cmd.config.ownerOnly && !client.config.instagram.ownerIds.includes(message.author.id)) return
+
+        try {
+            cmd.run({
+                client,
+                message,
+                args,
+                argsOptions,
+                data,
+                i18n
+            }).then(() => {
+                client.logger.commandLog({ message: cmd.help.name }) //message, prefix, cmd.help.name, messageTime, ((Date.now() - start) / 1000) - messageTime/)
+            }).catch(error => {
+                message.chat.sendMessage(i18n.__("error.errorOccured", { error: error.toString() }))
+
+                client.logger.error({ message: error })
+            })
+        } catch (error) {
+            message.chat.sendMessage(i18n.__("error.errorOccured", { error: error.toString() }))
+
+            client.logger.error({ message: error })
+        }
     }
 }
-
-/*
-
-    client.on("messageCreate", async(message) => {
-
-        if ([`${prefix}puissance4`, `${prefix}p4`, `${prefix}eval`].includes(message.content)) client.logger.commandLog({ 
-            interactionId: message.id,
-            commandName: command,
-            prefix: ""
-        })
-
-        if (message.content.startsWith(`${prefix}puissance4`) || message.content.startsWith(`${prefix}p4`)) {
-            let opponent
-            
-            try {
-                opponent = await client.fetchUser(args[0])
-            } catch(error) {
-                return await message.chat.sendMessage("Aucun utilisateur n'a été trouvé")
-            }
-
-            if (!message.chat.isGroup) return await message.chat.sendMessage("Oh non vous devez être dans un groupe pour effectuer cette commande")
-        
-            if (message.author.id === opponent.id) return await message.chat.sendMessage("Vous ne pouvez pas jouer contre vous meme !")
-
-            if (opponent.id === client.user.id) return await message.chat.sendMessage("Vous ne pouvez pas jouer contre moi")
-        
-            if (!message.chat.users.has(opponent.id)) return await message.chat.sendMessage("La personne doit être presente dans le groupe")
-
-            if (message.chat.puissance4) return await message.chat.sendMessage(`Désolé @${message.author.username} une partie est deja en cours`)
-
-            return opponentReady({ message, opponent })    
-        } else if (message.content.startsWith(`${prefix}eval`) && message.author.id === 18291915089) {
-            if (!args[0]) return await message.chat.sendMessage("Veuillez saisir un argument")
-            let toExecute = message.content.split(" ").slice(1)
-
-            if (argsOptions.length > 0) toExecute = toExecute.slice(0, toExecute.length - (argsOptions.length - 1))
-
-            const content = toExecute.join(" ")
-            const result = new Promise((resolve) => resolve(eval(content)))
-
-            return result.then(async(output) => {
-                if (typeof output !== "string") output = inspect(output, { depth: 0 })
-                
-                if (argsOptions[0] === "result" && !["yes", "true"].includes(argsOptions[1])) return 
-
-                return await message.chat.sendMessage(output)
-            }).catch(async(err) => {
-                err = err.toString()
-                
-                return await message.chat.sendMessage(err)
-            })
-        }
-    })
-*/
