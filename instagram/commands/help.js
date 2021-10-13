17555cf74701eef769ce5460405a7a7783133195
Superalexandre <superalexandre0@gmail.com>
2021-10-13 22:12:06 +0200

    [+] Create help command

--- instagram/commands/help.js
+++ instagram/commands/help.js
@@ -13,6 +13,30 @@ export default class Help extends Command {
     }
 
     async run({ client, message, args, argsOptions, data, i18n }) {
-        return message.chat.sendMessage("En cours")
+
+        if (args[0]) {
+            const command = client.commands.get(args[0].toLowerCase()) || client.commands.get(client.aliases.get(args[0].toLowerCase()))
+
+            if (!command) return message.chat.sendMessage(`Aucune commande trouvée pour ${args[0]}`)
+
+            if (!command.config.enabled) return
+            if (command.config.ownerOnly && !client.config.instagram.ownerIds.includes(message.author.id.toString())) return
+
+            message.chat.sendMessage(`Informations sur la commande : ${command.help.name}\n\n
+            • Alias : ${command.help.aliases.join(", ")}\n
+            • Description : ${command.help.desc(i18n)}\n
+            • Usage : ${command.help.use(i18n)}\n
+            • Exemple : ${command.help.example(i18n)}`)
+        } else {
+            const commandList = []
+            
+            for (const [commandName, commandData] of client.commands) {
+                if (commandData.config.ownerOnly && !client.config.instagram.ownerIds.includes(message.author.id.toString())) continue
+
+                commandList.push(`• ${client.config.instagram.prefix}${commandName}`)
+            }
+
+            message.chat.sendMessage(`Liste des commandes : \n` + commandList.join("\n"))
+        }
     }
 }
