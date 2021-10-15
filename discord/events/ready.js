01952c34c1f61cb8c3977035f3901db0afefb839
Alexandre <superalexandre0@gmail.com>
2021-10-15 22:37:50 +0200

    [+] Start to check command

--- discord/events/ready.js
+++ discord/events/ready.js
@@ -1,4 +1,5 @@
-//import fetch from "node-fetch"
+import fetch from "node-fetch"
+//import { SlashCommandBuilder } from "@discordjs/builders"
 
 export default class Ready {
     constructor(client) {
@@ -8,20 +9,38 @@ export default class Ready {
     async run() {
         const client = this.client
 
-        /*
         const config = client.config
         const apiUrl = "https://discord.com/api/v8"
         const endPoint = `/applications/${config.discord.appId}/commands`
         const commands = await fetch(`${apiUrl}${endPoint}`, {
             headers: { 
                 Authorization: `Bot ${config.discord.token}`
-            },
+            }
         })
-        const jsonCommands = await commands.json()
 
-        console.log(jsonCommands)
-        */
+        let slashCommandList = await commands.json()
+        let commandList = client.commands
+        
+        for (const [commandName, commandData] of commandList) {
+            if (!commandData.config.enabled) continue
+            if (slashCommandList.map(cmd => cmd.name === commandName).includes(commandName)) continue
+            
+            client.logger.warn({ message: `La commande ${commandName} n'est pas enregistrer !` })
 
-        client.logger.log({ message: `Client prêt (${client.user.username}#${client.user.discriminator})`  })
+            /*
+            const command = new SlashCommandBuilder()
+                .setName(commandData.help.name)
+                .setDescription(commandData.help.desc ?? "Aucune description fourni")
+
+            const rep = await fetch(``, {
+                method: "POST",
+                body: 
+            })
+
+            console.log(commandData.help.name)
+            */
+        }
+
+        client.logger.log({ message: `Client prêt (${client.user.username}#${client.user.discriminator})` })
     }
 }
