c83761a4a14e0175a97f70e850e5ebe73923fb75
Alexandre <superalexandre0@gmail.com>
2021-10-20 21:04:42 +0200

    [+] add gameStats function

--- functions.js
+++ functions.js
@@ -18,8 +18,8 @@ async function createAccount({ data, lang = "fr_FR", plateformData = {} }) {
         createdTimestamp: Date.now(),
         lang,
         plateformData,
-        statistics: {},
-        achievement: {}
+        statistics: [],
+        achievement: []
     })
 
     const account = data.users.get(id)
@@ -49,6 +49,39 @@ async function mergeAccount({ data, id1, id2 }) {
     return { success: true, error: false, newAccount }
 }
 
+async function gameStats({ data, plateform, user1, user2, gameName, winnerId }) {
+    if (!user1.id || !user2.id) {
+        new Error("No id provided in user object")
+    
+        return { success: false, error: true, message: "No id provided in user object" }
+    }
+
+    const user1Data = await data.users.find(user => user.plateformData.find(data => data.plateform === plateform && data.data.id === user1.id))
+    const user2Data = await data.users.find(user => user.plateformData.find(data => data.plateform === plateform && data.data.id === user2.id))
+
+    if (!user1Data || !user2Data) {
+        new Error("No account found")
+
+        return { success: false, error: true, message: "No account found" }
+    }
+
+    await data.users.push(user1Data.accountId, {
+        gameName,
+        date: Date.now(),
+        versus: user2,
+        result: winnerId === user1.id ? "win" : winnerId === user2.id ? "loose" : "equality"
+    }, "statistics")
+
+    await data.users.push(user2Data.accountId, {
+        gameName,
+        date: Date.now(),
+        versus: user1,
+        result: winnerId === user1.id ? "loose" : winnerId === user2.id ? "win" : "equality"
+    }, "statistics")
+
+    return { success: true, error: false, user1Data, user2Data }
+}
+
 function genId() {
     let random = ""
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
@@ -66,5 +99,6 @@ export default {
     createAccount,
     deleteAccount,
     genId,
+    gameStats,
     mergeAccount
 }
