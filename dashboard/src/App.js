2eb7eb07f0d884eff42ef14bd7c903160cc2a4c6
Alexandre <superalexandre0@gmail.com>
2021-10-21 22:16:57 +0200

    [-] Remove for new projet

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,30 +1,6 @@
-import { motion, AnimatePresence } from "framer-motion"
-import { useState } from "react"
-import Modal from "./components/Modal/index"
-
 function App() {
-	const [modalOpen, setModelOpen] = useState(false)
-
-	const closeModel = () => setModelOpen(false)
-	const openModel = () => setModelOpen(true)
-
 	return (
 		<div>
-			<motion.button
-				whileHover={{ scale: 1.1 }}
-				whileTap={{ scale: 0.9 }}
-				className="button"
-				onClick={() => (modalOpen ? closeModel() : openModel())}
-			>
-				Button test
-			</motion.button>
-
-			<AnimatePresence
-				initial={false}
-				exitBeforeEnter={true}
-			>
-				{modalOpen && <Modal modalOpen={modalOpen} handleClose={closeModel}/>}
-			</AnimatePresence>
 		</div>
     )
 }
