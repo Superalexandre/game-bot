85c736937e47b6903546510b23a925efc3a8c510
Superalexandre <superalexandre0@gmail.com>
2021-10-22 22:47:19 +0200

    [/] Update App

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,11 +1,16 @@
-import NavBar from "./components/NavBar/NavBar"
+import Home from "./Home.js"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+//https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
 
 function App() {
 	return (
-		<div class="">
-			<NavBar></NavBar>
-		</div>
-    )
+		<Router>
+			<Switch>
+				<Route path="/" exact component={Home} />
+			</Switch>
+		</Router>
+	)
 }
 
 export default App
