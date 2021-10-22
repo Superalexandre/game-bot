0b53a029513200dcf479e7d7ebbd20cb350b7e31
Superalexandre <superalexandre0@gmail.com>
2021-10-22 23:56:09 +0200

    [+] Add redirect 404 and login page

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,6 @@
 import Home from "./Home.js"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./Login.js"
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 
 //https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
 
@@ -8,6 +9,9 @@ function App() {
 		<Router>
 			<Switch>
 				<Route path="/" exact component={Home} />
+				<Route path="/login" component={Login} />
+
+				<Redirect to="/" />
 			</Switch>
 		</Router>
 	)
