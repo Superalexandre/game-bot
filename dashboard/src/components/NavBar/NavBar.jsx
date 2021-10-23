e4446919c7c2ea3b8a6af8e1ef541b5ce1628981
Superalexandre <superalexandre0@gmail.com>
2021-10-23 11:39:05 +0200

    [-] Remove comment

--- dashboard/src/components/NavBar/NavBar.jsx
+++ dashboard/src/components/NavBar/NavBar.jsx
@@ -56,18 +56,4 @@ const NavBar = () => {
     )
 }
 
-        /*
-        <div className="absolute h-full w-60 dark:bg-gray-secondary bg-gray md:left-0 -left-full">
-            <div>
-                <a href="/" className={className()}>Accueil</a>
-    
-                <a href="/login" className={className()}>Se connecter</a>
-            </div>
-            <div className="bottom-0 left-0 right-0 ml-auto mr-auto absolute p-11">
-                <img className="h-32 left-0 right-0 ml-auto mr-auto" src={Logo} alt="Logo"/>
-                <p className="text-white text-center p">Game Bot</p>
-            </div>
-        </div>
-        */
-
 export default NavBar
