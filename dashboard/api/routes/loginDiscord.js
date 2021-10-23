d942d40ce5e8404777b6d81a3286484170e748f7
Superalexandre <superalexandre0@gmail.com>
2021-10-23 11:54:34 +0200

    [/] Rename url

--- dashboard/api/routes/loginDiscord.js
+++ dashboard/api/routes/loginDiscord.js
@@ -1,8 +1,12 @@
 import express from "express"
 const router = express.Router()
 
-router.get("/api/loginDiscord", function(req, res, next) {
+router.get("/api/login/discord", function(req, res, next) {
     res.send('{ "result": true }')
+})
+
+router.get("/api/callback/discord", function(req, res, next) {
+    res.send("Call back de discord")
 })
 
 export default router
