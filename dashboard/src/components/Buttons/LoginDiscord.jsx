f1d207256e762cd7d64b17e488fd0e6ba49976d8
Superalexandre <superalexandre0@gmail.com>
2021-10-23 11:54:53 +0200

    [+] Add url

--- dashboard/src/components/Buttons/LoginDiscord.jsx
+++ dashboard/src/components/Buttons/LoginDiscord.jsx
@@ -1,6 +1,6 @@
 const Button = ({ text }) => {
     return (
-        <a href="/api/discordLogin">
+        <a href="https://discord.com/api/oauth2/authorize?client_id=848272310557343795&redirect_uri=localhost%3A300%2Fapi%2Fcallback%2Fdiscord&response_type=code&scope=identify%20email%20guilds">
             {text}
         </a>
     )
