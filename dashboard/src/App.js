96171fb712ffb59245997f4d9e34fd7ccf20b305
Superalexandre <superalexandre0@gmail.com>
2021-10-23 11:38:23 +0200

    [/] Function => Class

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,20 +1,23 @@
 import Home from "./pages/Home.js"
 import Login from "./pages/Login.js"
+import { Component } from "react"
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 
 //https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
 
-function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route path="/" exact component={Home} />
-				<Route path="/login" component={Login} />
+class App extends Component {
+	render() {	
+		return (
+			<Router>
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/login" component={Login} />
 
-				<Redirect to="/" />
-			</Switch>
-		</Router>
-	)
+					<Redirect to="/" />
+				</Switch>
+			</Router>
+		)
+	}
 }
 
 export default App
