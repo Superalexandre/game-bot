f465e471d714571e90eab32700d4eb05fdfa410d
Superalexandre <superalexandre0@gmail.com>
2021-10-23 11:37:56 +0200

    [/] Function => Class

--- dashboard/src/pages/Login.js
+++ dashboard/src/pages/Login.js
@@ -1,15 +1,28 @@
 import NavBar from "../components/NavBar/NavBar"
+import LoginButtonDiscord from "../components/Buttons/LoginDiscord"
+import { Component } from "react"
 
-function Login() {
-    return (
-		<div className="top-0 left-0 absolute w-full h-full dark">
-            <NavBar></NavBar>
+class Login extends Component { 
+    render() {
+        return (
+		    <div className="top-0 left-0 absolute w-full h-full dark">
+                <NavBar></NavBar>
 
-            <div className="dark:bg-gray-primary bg-white w-full h-full">
-                <p>Login</p>
+                <div className="dark:bg-gray-primary bg-white w-full h-full">
+                    
+                    <div>
+
+                        <LoginButtonDiscord
+                            text="Connectez vous avec Discord"
+                        >
+                        </LoginButtonDiscord>
+
+                    </div>
+
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Login
