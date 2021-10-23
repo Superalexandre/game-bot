2fc5c1293223dab3d8b581633317fa7979b0e179
Superalexandre <superalexandre0@gmail.com>
2021-10-23 13:58:44 +0200

    [-] Remove router

--- dashboard/api/routes/loginDiscord.js
+++ dashboard/api/routes/loginDiscord.js
@@ -1,12 +1,4 @@
 import express from "express"
 const router = express.Router()
 
-router.get("/api/login/discord", function(req, res, next) {
-    res.send('{ "result": true }')
-})
-
-router.get("/api/callback/discord", function(req, res, next) {
-    res.send("Call back de discord")
-})
-
 export default router
