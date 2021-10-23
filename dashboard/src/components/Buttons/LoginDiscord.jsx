e6118de11f62355ee15914519d9ca7828e8daf05
Superalexandre <superalexandre0@gmail.com>
2021-10-23 14:01:20 +0200

    [+] Add div add two port

--- dashboard/src/components/Buttons/LoginDiscord.jsx
+++ dashboard/src/components/Buttons/LoginDiscord.jsx
@@ -1,8 +1,13 @@
 const Button = ({ text }) => {
     return (
-        <a href="https://discord.com/api/oauth2/authorize?client_id=848272310557343795&redirect_uri=localhost%3A300%2Fapi%2Fcallback%2Fdiscord&response_type=code&scope=identify%20email%20guilds">
-            {text}
-        </a>
+        <div>
+            <a href="https://discord.com/api/oauth2/authorize?client_id=848272310557343795&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fapi%2Fcallback%2Fdiscord&response_type=code&scope=identify%20email%20guilds">
+                {text} 5000
+            </a>
+            <a href="https://discord.com/api/oauth2/authorize?client_id=848272310557343795&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fcallback%2Fdiscord&response_type=code&scope=identify%20email%20guilds">
+                {text} 3000
+            </a>
+        </div>
     )
 }
 
