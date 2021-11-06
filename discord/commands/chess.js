aefb90d88ed0446f8685c89dfaf2fae6b578a6df
Superalexandre <superalexandre0@gmail.com>
2021-11-06 13:37:00 +0100

    [+] Add move system, edit image, create users data

--- discord/commands/chess.js
+++ discord/commands/chess.js
@@ -1,6 +1,7 @@
 import { Command } from "../structures/Command.js"
 import { Chess as ChessModule } from "chess.js"
 import Canvas from "canvas"
+import { MessageEmbed } from "discord.js"
 
 export default class Chess extends Command {
     constructor(client) {
@@ -11,29 +12,9 @@ export default class Chess extends Command {
         })
     }
 
-    async run({ interaction }) {
-        const chess = new ChessModule()
-        const board = chess.board()
-
-        const pieces = {
-            black: {
-                p: "./assets/images/chess/black-pawn.png", // Pawn
-                r: "./assets/images/chess/black-rook.png", // Rook
-                n: "./assets/images/chess/black-knight.png", // Knight
-                b: "./assets/images/chess/black-bishop.png", // Bishop
-                q: "./assets/images/chess/black-queen.png", // Queen
-                k: "./assets/images/chess/black-king.png" // King
-            },
-            white: {
-                p: "./assets/images/chess/white-pawn.png", // Pawn
-                r: "./assets/images/chess/white-rook.png", // Rook
-                n: "./assets/images/chess/white-knight.png", // Knight
-                b: "./assets/images/chess/white-bishop.png", // Bishop
-                q: "./assets/images/chess/white-queen.png", // Queen
-                k: "./assets/images/chess/white-king.png" // King
-            }
-        }
-
+    async run({ client, interaction, i18n, options }) {
+        const opponent = options.getUser("adversaire")
+        /*
         const boardCanvas = await genBoard({ board, pieces })
 
         await interaction.channel.send({
@@ -43,8 +24,128 @@ export default class Chess extends Command {
                 name: "chess.png"
             }]
         })
+        */
 
+        return opponentReady({ client, interaction, i18n, opponent })
     }
+}
+
+async function opponentReady({ i18n, interaction, opponent, client }) {
+    const msg = await interaction.channel.send("Tout les utilisateurs sont pret")
+
+    return startGame({ i18n, interaction, msg, opponent, client })
+}
+
+async function startGame({ i18n, interaction, msg, opponent, client }) {
+    let userData = {
+        username: interaction.user.username,
+        id: interaction.user.id,
+        turn: true,
+        color: "w",
+        colorName: "white"
+    }
+    
+    let opponentData = {
+        username: opponent.username,
+        id: opponent.id,
+        turn: false,
+        color: "b",
+        colorName: "black"
+    }
+
+    const gameData = {
+        date: Date.now(),
+        players: [userData, opponentData],
+        actions: []
+    }
+    
+    const chess = new ChessModule()
+    const pieces = {
+        black: {
+            p: "./assets/images/chess/black-pawn.png", // Pawn
+            r: "./assets/images/chess/black-rook.png", // Rook
+            n: "./assets/images/chess/black-knight.png", // Knight
+            b: "./assets/images/chess/black-bishop.png", // Bishop
+            q: "./assets/images/chess/black-queen.png", // Queen
+            k: "./assets/images/chess/black-king.png" // King
+        },
+        white: {
+            p: "./assets/images/chess/white-pawn.png", // Pawn
+            r: "./assets/images/chess/white-rook.png", // Rook
+            n: "./assets/images/chess/white-knight.png", // Knight
+            b: "./assets/images/chess/white-bishop.png", // Bishop
+            q: "./assets/images/chess/white-queen.png", // Queen
+            k: "./assets/images/chess/white-king.png" // King
+        }
+    }
+
+    const boardCanvas = await genBoard({ board: chess.board(), pieces })
+
+    const embed = new MessageEmbed()
+        .setTitle("Echec test")
+        .setImage("attachment://chess.png")
+
+    await msg.edit({
+        content: null,
+        attachments: [],
+        files: [{
+            attachment: boardCanvas.canvas.toBuffer(),
+            name: "chess.png"
+        }],
+        embeds: [embed]
+    })
+
+    const filter = (message) => [userData.id, opponentData.id].includes(message.author.id)
+    const collector = await interaction.channel.createMessageCollector({ filter })
+
+    collector.on("collect", async(message) => {
+        const regex = /\b[A-H]{1}[1-9]{1};{1}[A-H]{1}[1-9]\b/gi
+        
+        let activeUser = userData.id === message.author.id ? userData : opponentData
+        let opposite = userData.id === message.author.id ? opponentData : userData
+
+        if (!activeUser.turn) return
+
+        //Test message content with regex
+        if (regex.test(message.content)) {
+            const [from, to] = message.content.split(";")
+
+            const piece = chess.get(from)
+
+            if (!piece) return await msg.edit({
+                content: `Aucune piece en ${from}`
+            })
+
+            if (piece && piece.color !== activeUser.color) return await msg.edit({
+                content: `Cette piece n'est pas de votre couleur`
+            })
+
+            //Test if move is valid
+            if (chess.move({ from, to })) {
+
+                //Edit embed image with new board
+                const boardCanvas = await genBoard({ board: chess.board(), pieces })
+
+                const embed = new MessageEmbed()
+                    .setTitle("Echec test 2")
+                    .setImage("attachment://chess.png")
+
+                return await msg.edit({
+                    content: null,
+                    attachments: [],
+                    files: [{
+                        attachment: boardCanvas.canvas.toBuffer(),
+                        name: "chess.png"
+                    }],
+                    embeds: [embed]
+                })
+            }
+        }
+
+        await msg.edit({
+            content: "Ce mouvement n'est pas valide"
+        })
+    })
 }
 
 async function genBoard({ board, pieces }) {
@@ -63,7 +164,7 @@ async function genBoard({ board, pieces }) {
                 //Writes numbers from 1 to 8 with i
                 ctx.fillStyle = "#FFFFFF"
                 ctx.font = "bold 20px Arial"
-                ctx.fillText(i + 1, j * size + size / 2, (i + 1) * size + (size / 2) + 10)
+                ctx.fillText(board.length - i, j * size + size / 2, (i + 1) * size + size / 2)
             }
             
             const letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
